16b3fe9d98f74df1c8ea336f3d6895f0e7a5b2a4
kalomoio <i.kalomoiris@afse.eu>
2018-06-02 19:39:04 +0300

    Some changes

--- src/com/kalomiris/Main.java
+++ src/com/kalomiris/Main.java
@@ -15,41 +15,43 @@ import java.util.Date;
 public class Main {
 
     public static StockServiceImpl stockServiceImpl = new StockServiceImpl();
-    public static TradeServiceImpl tradeServiceImpl = new TradeServiceImpl();
     private static BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         boolean quit = false;
         while (!quit) {
 
-            int firstAction;
-            System.out.println("Press 1 for stock trade or 2 for calculating the GBCE All Shares.\n");
-            firstAction = Integer.parseInt(scanner.readLine());
+            System.out.println("Press -1- for stock trade or -2- for calculating the GBCE All Shares\n" +
+                    "-0- to quit.\n");
+            int firstAction = enterAction();
 
             switch (firstAction) {
 
+                case 0:
+
+                    /** Quit from application  */
+                    quit = true;
+                    System.out.println("\n\nShout-down application...");
+                    break;
+
                 case 1:
 
-                    /**	For a given stock   */
+                    /**    For a given stock   */
 
                     Stock newStock = chooseStock();
 
-                    int action = enterAction();
+                    int secondAction = enterAction();
 
-                    switch (action) {
-                        case 0:
+                    switch (secondAction) {
 
-                            /** Quit from application  */
-                            quit = true;
-                            System.out.println("\n\nShout-down application...");
-                            break;
                         case 1:
 
                             /**  Compute dividend yield  */
                             double dividendYield = stockServiceImpl.calculateDividendYield(newStock);
                             System.out.println("Dividend yield: " + dividendYield);
                             break;
+
                         case 2:
 
                             /** Compute P/E Ratio  */
@@ -57,61 +59,55 @@ public class Main {
                             if (ration != 0.0) {
                                 System.out.println("Ratio: " + ration);
                             } else {
-                                System.out.println("Impossibility.");
+                                System.out.println("Try again...");
                             }
                             break;
+
                         case 3:
 
                             /** Record a trade  */
-                            System.out.println("\n\n" + newStock. + "\nGiven symbol of stock: " + stockSymbol + "\nQuantity: " +
-                                    myQuantity + "\nIs " + indicator1 + "\nPrice: " + myTradePrice);
+                            Trade currentTrade = newStock.getTradeList().get(1);   //Choose (randomly) one of two trade of specific stock.
+                            System.out.println("\n\n" + currentTrade.getDate() +
+                                                "\nGiven symbol of stock: " + newStock.getSymbol() +
+                                                "\nQuantity: " + currentTrade.getQuantity() +
+                                                "\nIs " + currentTrade.getIndicator() +
+                                                "\nPrice: " + currentTrade.getTradePrice());
                             break;
+
                         case 4:
 
                             /**Compute the price of stock for all trade in past 15 min  */
                             double stockPrice = stockServiceImpl.calculateStockPriceTime(newStock, 15);
                             System.out.println("\n\nThe stock price in the 15 last minute is: " + stockPrice);
                             break;
+
                         case 5:
+
                             printActions();
                             break;
+
                         default:
-                            System.out.println("Wrong input...\n");
+
+                            System.out.println("Wrong input, press 1 to 5...\n");
                     }
 
                     break;
 
                 case 2:
 
-                    /** 	Calculate the GBCE All Share Index using
+                    /**    Calculate the GBCE All Share Index using
                      * the geometric mean of prices for all stocks  */
 
                     double goeMeans = stockServiceImpl.calculateGeoMean();
                     System.out.println("The Geometric mean of all stocks is: " + goeMeans);
                     break;
+
                 default:
-                    System.out.println("Wrong input...\n");
+
+                    System.out.println("Wrong input, press 1 or 2...\n");
             }
         }
     }
-
-//    public static Trade findTrade() throws IOException {
-//        String indicator;
-//        Trade newTrade;
-//
-//        do {
-//            System.out.println("Press b (buy) if your trade is buy else press s (sell): ");
-//            indicator = scanner.readLine();
-//            //Restrict input...
-//            if ("s".equals(indicator) || "b".equals(indicator)) {
-//                break;
-//            } else {
-//                System.out.println("Wrong input, press b or s...");
-//            }
-//        } while (true);
-//
-//        return newTrade;
-//    }
 
     public static Stock chooseStock() {
         do {
@@ -130,7 +126,7 @@ public class Main {
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (NumberFormatException e) {
-                System.out.println("\nYou can press only numbers, please try again...\n\n");
+                System.out.println("\nYou can press only the given symbol, please try again...\n\n");
             }
         } while (true);
     }
@@ -138,14 +134,8 @@ public class Main {
     public static int enterAction() {
         System.out.println("\n...Enter action: ");
         do {
-            printActions();
             try {
                 int action = Integer.parseInt(scanner.readLine());
-                if (action >= 0 && action <= 6) {
-                    return action;
-                } else {
-                    System.out.println("Please try again...");
-                }
             } catch (IOException e) {
                 System.out.println("\nPlease try again...\n");
             } catch (WrongNumberException m) {
@@ -158,8 +148,7 @@ public class Main {
 
     private static void printActions() {
         System.out.println("\nAvailable actions: \npress");
-        System.out.println("0  -to shutdown\n" +
-                "1  -to calculate the dividend yield\n" +
+        System.out.println("1  -to calculate the dividend yield\n" +
                 "2  -to calculate the P/E Ratio\n" +
                 "3  -to record a trade\n" +
                 "4  -to calculate Stock Price based on trades recorded in past 15 minutes\n" +
