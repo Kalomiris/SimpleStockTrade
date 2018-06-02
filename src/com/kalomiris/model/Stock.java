c13b1d8cb3e884cab917395a31e16bbaef0f21c0
kalomoio <i.kalomoiris@afse.eu>
2018-06-02 19:39:38 +0300

    Stock model

---
+++ src/com/kalomiris/model/Stock.java
@@ -0,0 +1,76 @@
+package com.kalomiris.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stock {
+
+
+    public enum Type {COMMON, PREFERRED}
+    Type type;
+
+    private String symbol;
+    private int lastDividend;
+    private int fixedDividend;
+    private int parValue;
+    private double tickerPrice;
+    private List<Trade> tradeList = new ArrayList<>();
+
+    public Stock(String symbol,
+                 int lastDividend,
+                 int fixedDividend,
+                 int parValue,
+                 double tickerPrice,
+                 Type type) {
+        this.symbol = symbol;
+        this.lastDividend = lastDividend;
+        this.fixedDividend = fixedDividend;
+        this.parValue = parValue;
+        this.tickerPrice = tickerPrice;
+        this.type = type;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getLastDividend() {
+        return lastDividend;
+    }
+
+
+    public double getTickerPrice() {
+        return tickerPrice;
+    }
+
+    public List<Trade> getTradeList() {
+        return tradeList;
+    }
+
+    public int getFixedDividend() {
+        return fixedDividend;
+    }
+
+    public int getParValue() {
+        return parValue;
+    }
+
+    public void setTradeList(List<Trade> tradeList) {
+        this.tradeList = tradeList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Stock stock = (Stock) o;
+
+        return symbol.equals(stock.symbol);
+    }
+
+    @Override
+    public int hashCode() {
+        return symbol.hashCode();
+    }
+}
