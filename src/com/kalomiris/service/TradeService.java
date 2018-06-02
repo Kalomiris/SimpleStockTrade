64f4649f097c9b583da0f37e43a0b28ade6b6cb0
kalomoio <i.kalomoiris@afse.eu>
2018-06-02 19:37:24 +0300

    Interface

---
+++ src/com/kalomiris/service/TradeService.java
@@ -0,0 +1,11 @@
+package com.kalomiris.service;
+
+import com.kalomiris.model.Trade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface TradeService {
+
+    //void addRecord (Trade trade);
+}
