30a0a08c11008f5ca8be8d178aa1cd5d0d74b8be
kalomoio <i.kalomoiris@afse.eu>
2018-06-02 19:31:40 +0300

    Interface

---
+++ src/com/kalomiris/service/StockService.java
@@ -0,0 +1,14 @@
+package com.kalomiris.service;
+
+import com.kalomiris.model.Stock;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface StockService {
+    List<Double> stockPriceList = new ArrayList<>();
+     double calculateDividendYield(Stock stock);
+     double calculateRatio(Stock stock);
+     double calculateStockPrice(Stock stock);
+     double calculateGeoMean();
+     double calculateStockPriceTime(Stock stock, int time);
+}
